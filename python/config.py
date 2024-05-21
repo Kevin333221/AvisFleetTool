c818cd222838ce71ae40e0d38b38e8e4195bf07d
kevin333221 <kevinmathias@hotmail.no>
2024-05-21 20:36:56 +0200

    Trying Excel!

---
+++ python/config.py
@@ -0,0 +1,95 @@
+    
+cursor_locations = {
+    "CMD_LINE": 7,
+    
+    "x502_PAC": 82,
+    
+    "x515_ACTION": 89,
+    "x515_RPT": 98,
+    "x515_PAGE_NUM": 474,
+    
+    "x601_ACTION": 91,
+    "x801_ACTION": 171,
+    
+    "x313_ACTION": 170,
+    "x313_MVA_NO": 330,
+    "x313_REG_NO": 385,
+    "x313_FLEET_CODE": 495,
+    "x313_MAKE": 521,
+    "x313_IGNIT_KEY": 546,
+    "x313_CURR_LOC": 575,
+    "x313_TRUNK_KEY": 626,
+    "x313_LAST_MOVEMENT": 655,
+    "x313_BODY_TYPE": 681,
+    "x313_MILES": 735,
+    "x313_CAR_GROUP": 761,
+    "x313_LOCATION_OUT": 815,
+    "x313_MOVEMENT": 895,
+    "x313_DATE_OUT": 975,
+    "x313_DATE_DUE": 1055,
+    "x313_ACCESSORY": 1081,
+    "x313_LOCATION_DUE": 1135,
+    "x313_FUEL_TYPE": 1401,
+    "x313_STATUS": 1615,
+    "x313_HOLD_DATE": 1695,
+    "x313_HOLD_REASON": 1775,
+}
+
+columns = {
+    "name":     (0, 19),
+    "arr_time": (20, 24),
+    "flight_no":(25, 31),
+    "car_grp":  (33, 34),
+    "rate_code":(37, 41),
+    "remarks":  (43, 65),
+    "exp_lor":  (70, 73),
+    "in_sta":   (74, 77)
+}
+
+months = {
+    "JAN": 31,
+    "FEB": 28,
+    "MAR": 31,
+    "APR": 30,
+    "MAY": 31,
+    "JUN": 30,
+    "JUL": 31,
+    "AUG": 31,
+    "SEP": 30,
+    "OCT": 31,
+    "NOV": 30,
+    "DEC": 31
+}
+
+num_to_months = {
+    1: "JAN",
+    2: "FEB",
+    3: "MAR",
+    4: "APR",
+    5: "MAY",
+    6: "JUN",
+    7: "JUL",
+    8: "AUG",
+    9: "SEP",
+    10: "OCT",
+    11: "NOV",
+    12: "DEC"
+}
+
+months_to_num = {
+    "JAN": 1,
+    "FEB": 2,
+    "MAR": 3,
+    "APR": 4,
+    "MAY": 5,
+    "JUN": 6,
+    "JUL": 7,
+    "AUG": 8,
+    "SEP": 9,
+    "OCT": 10,
+    "NOV": 11,
+    "DEC": 12
+}
+
+stations_A = ["TOS", "TR7"]
+stations_B = ["TOS", "T1Y"]
