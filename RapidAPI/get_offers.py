0e0d8f0582b08714dab94bff7d28becd82e05e68
Kevin Bergan <kevinmathias@hotmail.no>
2025-02-09 15:58:31 +0100

    small fix

--- RapidAPI/get_offers.py
+++ RapidAPI/get_offers.py
@@ -2,9 +2,9 @@ import requests
 
 url = "https://sky-scrapper.p.rapidapi.com/api/v1/cars/searchCars"
 
-pickup_date = "2024-12-23"
+pickup_date = "2025-01-13"
 pickup_time = "10:00"
-dropoff_date = "2024-12-31"
+dropoff_date = "2025-01-20"
 dropoff_time = "10:00"
 
 querystring = {
--- python/HLLAPI.py
+++ python/HLLAPI.py
@@ -2320,28 +2320,62 @@ def get_car_manifest(station, date):
     for car_group in onHand.split(","):
         car_groups_on_hand[car_group[1].upper()] = int(car_group[0])
     
-    def convert_time(time):
-        return int(time[0:2]) + int(time[2:4])/60
+    # def convert_time(time):
+    #     return int(time[0:2]) + int(time[2:4])/60
     
-    # RULES
-    # 1. If a customer has requested a car group that is not on hand, the customer will be assigned to the next available car group in the fill order 
-    #    IF AND ONLY IF the customer has a pickup time that is 2 hours until the next customer of that next available car group.
-    # 2. If a customer has requested a car group that is not on hand, the customer will be assigned to the next available car group in the fill order
-    #    IF AND ONLY IF the customer has a rental length that is less that 7 days.
-    # 3. If a customer has requested a car group that is not on hand, the customer will be assigned to the next available car group in the fill order
-    #    IF AND ONLY IF the customer has the same car group as the a next customer within 2 hours, if that next customer also needs a upgrade, it is better to 
-    #    upgrade each once instead of upgrading one customer twice.
-    
-    for car_group, count in car_groups_on_hand.items():
-        for _ in range(count):
-            for customer in cleaned_customers:
-                if customer.assignment is not None:
-                    continue
-                if customer.car_group == car_group:
-                    customer.assignment = car_group
-                    break
+     # Function to calculate the score
+    def calculate_score(pickup_time, rental_length):
+        return pickup_time + (rental_length // SCORE_PER_RENTAL_LENGTH)
+
+    # Function to get the car group index
+    def get_car_group_index(car_group):
+        return CAR_GROUP_FILL_ORDER.index(car_group) if car_group in CAR_GROUP_FILL_ORDER else EL_CAR_GROUP_FILL_ORDER.index(car_group)
+
+    # Iterate through the customers and assign cars
+    for i, customer in enumerate(cleaned_customers):
+        pickup_time, car_group, rental_length = customer.pickup_time, customer.car_group, customer.rental_length
+        car_group_index = get_car_group_index(car_group)
+        group = CAR_GROUP_FILL_ORDER if car_group in CAR_GROUP_FILL_ORDER else EL_CAR_GROUP_FILL_ORDER
+
+        # Check if there are available cars in the desired car group
+        if car_groups_on_hand.get(car_group, 0) > 0:
+            car_groups_on_hand[car_group] -= 1
+            customer.assignment = "OK"
+            customer.score = f"Score: {calculate_score(int(pickup_time), int(rental_length))}"
+        else:
+            # Check for possible upgrades
+            assigned = False
+            for j in range(car_group_index + 1, len(group)):
+                upgrade_car_group = group[j]
                 
-    print(f"Time - Car Group - Rental Length - Score - Assignment")
+                # Check if there are available cars in the desired car group
+                if car_groups_on_hand.get(upgrade_car_group, 0) > 0:
+                    
+                    for k in range(i + 1, len(cleaned_customers)):
+                        # print(f"Current: {customer} - Next: {cleaned_customers[k]}")
+                        next_customer_pickup_time = cleaned_customers[k].pickup_time
+                        if cleaned_customers[k].car_group in [car_group, upgrade_car_group]:
+                            current_customer_score = calculate_score(int(pickup_time), int(rental_length))
+                            next_customer_score = calculate_score(int(next_customer_pickup_time), int(cleaned_customers[k].rental_length))
+                            if next_customer_score >= current_customer_score + 1000:
+                                # print(f"Current: {current_customer_score} - Next: {next_customer_score}")
+                                car_groups_on_hand[upgrade_car_group] -= 1
+                                customer.assignment = f"Upgrader til {upgrade_car_group}"
+                                customer.score = f"Score: {calculate_score(int(pickup_time), int(rental_length))}"
+                                assigned = True
+                                break
+                    else:
+                        car_groups_on_hand[upgrade_car_group] -= 1
+                        customer.assignment = f"Upgrader til {upgrade_car_group}"
+                        customer.score = f"Score: {calculate_score(int(pickup_time), int(rental_length))}"
+                        assigned = True
+                        break
+                    
+            if not assigned:
+                customer.assignment = "Mangler"
+                customer.score = f"Score: {calculate_score(int(pickup_time), int(rental_length))}"                   
+
+    print("Give these customers the following cars:")
     for customer in cleaned_customers:
         print(customer)
     
@@ -2356,7 +2390,7 @@ def get_car_manifest(station, date):
 
 # get_car_group_availability_for_month(["DEC", "JAN"], ["TOS", "TR7"], ["TOS", "T1Y"])
 
-get_car_manifest("TOS", "13JAN2025")
+# get_car_manifest("TOS", "12JAN2025")
 
 # check_multiple_rentals_on_same_vehicle()
 
